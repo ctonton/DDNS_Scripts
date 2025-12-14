95f20d1f2c5b3b2199465b046d7986f1f27869a8
ctonton <51173996+ctonton@users.noreply.github.com>
2025-12-13 23:19:46 -0600

    Refactor DDNS update script for Cloudflare

--- cloudflare.sh
+++ cloudflare.sh
@@ -44,17 +44,17 @@ if [[ $OPT == i ]] ; then
   else
     sudo tee -a /opt/ddns.sh &>/dev/null <<EOT
 OLD_4=$NEW_4
-NEW_4=!!!(dig @1.1.1.1 whoami.cloudflare txt ch -4 +short +tries=1 | sed '/;;/d;s/"//g')
-if [[ !!!OLD_4 == !!!NEW_4 ]] ; then
+NEW_4=##(dig @1.1.1.1 whoami.cloudflare txt ch -4 +short +tries=1 | sed '/;;/d;s/"//g')
+if [[ ##OLD_4 == ##NEW_4 ]] ; then
   curl -s https://api.cloudflare.com/client/v4/zones/$ZON/dns_records/$REC_4 -X PATCH -H 'Content-Type: application/json' -H "Authorization: Bearer $TOK" -d '{
     "name": "$NAM_4",
     "ttl": 1,
     "type": "A",
     "comment": "Domain verification record",
-    "content": "'"!!!NEW_4"'",
+    "content": "'"##NEW_4"'",
     "proxied": false
   }' | grep -q '"success":true'
-  [[ !!!? -eq 0 ]] && sed -i "s/^OLD_4.*/OLD_4=!!!NEW_4/" !!!0
+  [[ ##? -eq 0 ]] && sed -i "s/^OLD_4.*/OLD_4=##NEW_4/" ##0
 fi
 EOT
   fi
@@ -63,22 +63,22 @@ EOT
   else
     sudo tee -a /opt/ddns.sh &>/dev/null <<EOT
 OLD_6=$NEW_6
-NEW_6=!!!(dig @2606:4700:4700::1111 whoami.cloudflare txt ch -6 +short +tries=1 | sed '/;;/d;s/"//g')
-if [[ !!!OLD_6 != !!!NEW_6 ]] ; then
+NEW_6=##(dig @2606:4700:4700::1111 whoami.cloudflare txt ch -6 +short +tries=1 | sed '/;;/d;s/"//g')
+if [[ ##OLD_6 != ##NEW_6 ]] ; then
   curl -s https://api.cloudflare.com/client/v4/zones/$ZON/dns_records/$REC_6 -X PATCH -H 'Content-Type: application/json' -H "Authorization: Bearer $TOK" -d '{
     "name": "$NAM_6",
     "ttl": 1,
     "type": "AAAA",
     "comment": "Domain verification record",
-    "content": "'"!!!NEW_6"'",
+    "content": "'"##NEW_6"'",
     "proxied": false
   }' | grep -q '"success":true'
-  [[ !!!? -eq 0 ]] && sed -i "s/^OLD_6.*/OLD_6=!!!NEW_6/" !!!0
+  [[ ##? -eq 0 ]] && sed -i "s/^OLD_6.*/OLD_6=##NEW_6/" ##0
 fi
 EOT
   fi
   echo "exit 0" | sudo tee -a /opt/ddns.sh &>/dev/null
-  sudo sed -i 's/!!!/$/g' /opt/ddns.sh
+  sudo sed -i 's/##/$/g' /opt/ddns.sh
   sudo chmod +x /opt/ddns.sh
   (sudo crontab -l 2>/dev/null | grep -v 'ddns.sh' ; echo "*/10 * * * * /opt/ddns.sh &>/dev/null") | sudo crontab -
   sudo systemctl restart cron
