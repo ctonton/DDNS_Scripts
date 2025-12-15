f7a6f081c09a6ee2de0e9671ac79e7a239ba1dd3
ctonton <51173996+ctonton@users.noreply.github.com>
2025-12-14 18:34:07 -0600

    Replace tr with sed for IP address extraction

--- duckdns.sh
+++ duckdns.sh
@@ -18,8 +18,8 @@ curl -V &>/dev/null && dig -v &>/dev/null
 # variables
 echo ; read -p "Enter the Token from Duck DNS: " TOK
 echo ; read -p "Enter the domain name to update: " DOM
-NEW_4=$(dig -4 TXT +short o-o.myaddr.l.google.com @ns1.google.com 2>/dev/null | tr -d '"')
-NEW_6=$(dig -6 TXT +short o-o.myaddr.l.google.com @ns1.google.com 2>/dev/null | tr -d '"')
+NEW_4=$(dig -4 TXT +short o-o.myaddr.l.google.com @ns1.google.com 2>/dev/null | sed '/;;/d;s/"//g')
+NEW_6=$(dig -6 TXT +short o-o.myaddr.l.google.com @ns1.google.com 2>/dev/null | sed '/;;/d;s/"//g')
 [ -z $NEW_4 ] && [ -z $NEW_6 ] && echo "Could not detect any public IP address" && exit 1
 
 # setup
@@ -45,7 +45,7 @@ EOT
   if [ ! -z $NEW_4 ] ; then
     sudo tee -a /opt/ddns.sh &>/dev/null <<EOT
 OLD_4=$NEW_4
-NEW_4=##(dig -4 TXT +short o-o.myaddr.l.google.com @ns1.google.com 2>/dev/null | tr -d '"')
+NEW_4=##(dig -4 TXT +short o-o.myaddr.l.google.com @ns1.google.com 2>/dev/null | sed '/;;/d;s/"//g')
 if [[ ##OLD_4 != ##NEW_4 || ##RUN == 1 ]] && [ ! -z ##NEW_4 ] ; then
   curl -s "https://www.duckdns.org/update?domains=$DOM&token=$TOK&ip=##NEW_4" | grep -q 'OK'
   [[ ##? -eq 0 ]] && sed -i "s/^OLD_4.*/OLD_4=##NEW_4/;s/^TTR.*/TTR=##((##(date +%s) + 604800))/" ##0
@@ -55,7 +55,7 @@ EOT
   if [ ! -z $NEW_6 ] ; then
     sudo tee -a /opt/ddns.sh &>/dev/null <<EOT
 OLD_6=$NEW_6
-NEW_6=##(dig -4 TXT +short o-o.myaddr.l.google.com @ns1.google.com 2>/dev/null | tr -d '"')
+NEW_6=##(dig -4 TXT +short o-o.myaddr.l.google.com @ns1.google.com 2>/dev/null | sed '/;;/d;s/"//g')
 if [[ ##OLD_6 != ##NEW_6 || ##RUN == 1 ]] && [ ! -z ##NEW_6 ] ; then
   curl -s "https://www.duckdns.org/update?domains=$DOM&token=$TOK&ip=##NEW_6" | grep -q 'OK'
   [[ ##? -eq 0 ]] && sed -i "s/^OLD_6.*/OLD_6=##NEW_6/;s/^TTR.*/TTR=##((##(date +%s) + 604800))/" ##0
