eae2cda5e851f6526de6def3e7ce1d6f8639ff9f
ctonton <51173996+ctonton@users.noreply.github.com>
2025-12-15 21:49:19 -0500

    Update duckdns.sh

--- duckdns.sh
+++ duckdns.sh
@@ -22,8 +22,8 @@ curl -V &>/dev/null && dig -v &>/dev/null
 # variables
 echo ; read -p "Enter the Token from Duck DNS: " TOK
 echo ; read -p "Enter the domain name to update: " DOM
-[[ $VER == 6 ]] || NEW_4=$(dig @1.1.1.1 whoami.cloudflare txt ch -4 +short +tries=1 | sed '/;;/d;s/"//g')
-[[ $VER == 4 ]] || NEW_6=$(dig @2606:4700:4700::1111 whoami.cloudflare txt ch -6 +short +tries=1 | sed '/;;/d;s/"//g')
+[[ $VER == 6 ]] || NEW_4=$(dig +short +tries=1 A myip.opendns.com @208.67.222.222 | sed '/^;;/d')
+[[ $VER == 4 ]] || NEW_6=$(dig +short +tries=1 AAAA myip.opendns.com @2620:119:35::35 | sed '/^;;/d')
 [[ -z $NEW_4 && -z $NEW_6 ]] && echo "No public IP address found." && exit 1
 
 # setup
@@ -53,8 +53,8 @@ EOT
 TTR=
 OLD_4=
 OLD_6=
-NEW_4=$(dig @1.1.1.1 whoami.cloudflare txt ch -4 +short +tries=1 | sed '/;;/d;s/"//g')
-NEW_6=$(dig @2606:4700:4700::1111 whoami.cloudflare txt ch -6 +short +tries=1 | sed '/;;/d;s/"//g')
+NEW_4=$(dig +short +tries=1 A myip.opendns.com @208.67.222.222 | sed '/^;;/d')
+NEW_6=$(dig +short +tries=1 AAAA myip.opendns.com @2620:119:35::35 | sed '/^;;/d')
 [[ -z $NEW_4 && -z $NEW_6 ]] && exit 1
 [[ $(date +%s) -ge $TTR ]] && RUN=1
 [[ $OLD_4 == $NEW_4 && $OLD_6 == $NEW_6 && $RUN != 1 ]] && exit 0
